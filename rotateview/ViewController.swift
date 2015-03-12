478fe4b6f1aebc61d763d0298e6deef9d9a5e861
chenzhe142 <chenzhe142@163.com>
2015-03-11 22:13:02 -0500

    Tested table view with video view when rotating screen.

--- rotateview/ViewController.swift
+++ rotateview/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var videoViewTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var videoViewBottomConstraint: NSLayoutConstraint!
+    
+    
+    @IBOutlet weak var resultViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var resultViewLeadingConstraint: NSLayoutConstraint!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func willRotateToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
+        let padding: CGFloat = 16.0
+        
+        // since we're calling this before the rotation, the height and width are swapped
+        let viewHeight = self.view.frame.size.width
+        let viewWidth = self.view.frame.size.height
+        
+        // if landscape
+        if UIInterfaceOrientationIsLandscape(toInterfaceOrientation) {
+            videoViewTrailingConstraint.constant = (viewWidth/2.0)
+            videoViewBottomConstraint.constant = 0
+            
+            
+            
+            resultViewLeadingConstraint.constant = (viewWidth/2.0)
+            resultViewTopConstraint.constant = 0
+            
+        } else { // else portrait
+            videoViewBottomConstraint.constant = (viewHeight/2.0)
+            videoViewTrailingConstraint.constant = -padding
+            
+            
+            resultViewLeadingConstraint.constant = -padding
+            resultViewTopConstraint.constant = (viewHeight/2.0)
+        }
+    }
 
 }
 
