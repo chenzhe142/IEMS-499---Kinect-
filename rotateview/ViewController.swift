837bd6ab23e9756608d6ae8d3c62403443f97a38
chenzhe142 <chenzhe142@163.com>
2015-03-12 12:57:21 -0500

    Tested view when video is playing on video view.

--- rotateview/ViewController.swift
+++ rotateview/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
     
+    //test data for custom cell
     let tableData = ["One","Two","Three"]
-    
     var arrayOfResults: [result] = [result]()
 
-
+    
+    //test ui for movieView
+    @IBOutlet weak var testPlayer: UIButton!
+    var moviePlayer: MPMoviePlayerController!
+    
+    
+    //view constraint, use to adjust the view when rotating screen.
     @IBOutlet weak var videoViewTrailingConstraint: NSLayoutConstraint!
     @IBOutlet weak var videoViewBottomConstraint: NSLayoutConstraint!
     
@@ -22,13 +29,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultViewTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var resultViewLeadingConstraint: NSLayoutConstraint!
     
-    
     @IBOutlet weak var searchParametersViewTrailingConstraint: NSLayoutConstraint!
     
     
     
-    
+    //ui view
     @IBOutlet weak var resultTableView: UITableView!
+    @IBOutlet weak var videoView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +43,28 @@ class ViewController: UIViewController {
         
         
         
+        let url = NSURL(string: "http://129.105.36.214/index2.php?StartTime=2015-02-13%2000:00:00&EndTime=2015-02-23%2023:00:00")
+        var error: NSError?
+        let html = NSString(contentsOfURL: url!, encoding: NSUTF8StringEncoding, error: &error)
+        
+        var str = (html as String).componentsSeparatedByString("<br>")
+        //println(str)
+        
+        var url_1: String = str[1]
+        url_1 = (url_1 as NSString).substringFromIndex(1)
+        //println(url_1)
+        
+        var baseurl = "http://129.105.36.214/webfile"
+        baseurl = baseurl + url_1
+        println(baseurl)
+        
+        var playurl:NSURL = NSURL(string: baseurl)!
+        
+        moviePlayer = MPMoviePlayerController(contentURL: playurl)
+        
+        
+        
+        //add data to arrayOfResults
         self.setupResults()
     }
 
@@ -54,12 +83,14 @@ class ViewController: UIViewController {
         
         // if landscape
         if UIInterfaceOrientationIsLandscape(toInterfaceOrientation) {
-            videoViewTrailingConstraint.constant = (viewWidth/2.0)
+            videoViewTrailingConstraint.constant = (viewWidth/2.0) - (padding)
             videoViewBottomConstraint.constant = 0
             
-            searchParametersViewTrailingConstraint.constant = (viewWidth/2.0)
+            moviePlayer.view.frame = self.videoView.bounds
             
-            resultViewLeadingConstraint.constant = (viewWidth/2.0)
+            searchParametersViewTrailingConstraint.constant = (viewWidth/2.0) - (padding)
+            
+            resultViewLeadingConstraint.constant = (viewWidth/2.0) - (padding)
             resultViewTopConstraint.constant = 0
             
         } else { // else portrait
@@ -102,6 +133,20 @@ class ViewController: UIViewController {
         var result1 = result(kinectNumber: 0, startTime: "20202020", endTime: "2020202", videoPath: "blank")
         arrayOfResults.append(result1)
     }
+    
+    
+    @IBAction func playMovie(sender: UIButton) {
+        moviePlayer.view.frame = self.videoView.bounds
+        moviePlayer.scalingMode = MPMovieScalingMode.AspectFit
+        moviePlayer.controlStyle = MPMovieControlStyle.Embedded
+        moviePlayer.shouldAutoplay = true
+        
+        videoView.addSubview(moviePlayer.view)
+        moviePlayer.prepareToPlay()
+        moviePlayer.play()
+    }
+    
+    
     
     
 }
