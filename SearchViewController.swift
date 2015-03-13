6b67249e0584b33a0f7f5a741d94215f908f7d7a
chenzhe142 <chenzhe142@163.com>
2015-03-12 22:01:10 -0500

    Add several text fields.

---
+++ SearchViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SearchViewController.swift
+//  rotateview
+//
+//  Created by 陈喆 on 15/3/12.
+//  Copyright (c) 2015年 Zhe Chen. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    
+    @IBOutlet weak var startDateTextField: UITextField!
+    @IBOutlet weak var startTimeTextField: UITextField!
+    @IBOutlet weak var endDateTextField: UITextField!
+    @IBOutlet weak var endTimeTextField: UITextField!
+    
+    @IBOutlet weak var roomNumberTextField: UITextField!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ rotateview/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  rotateview
+//
+//  Created by 陈喆 on 15/3/12.
+//  Copyright (c) 2015年 Zhe Chen. All rights reserved.
+//
+
+import Cocoa
+
+class SearchViewController: UIViewController {
+
+}
